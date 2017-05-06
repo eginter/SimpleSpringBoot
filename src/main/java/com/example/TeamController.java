9a84286e2b7e1a3c8b2873c724398782b2862491
elijah <ginterelijah@gmail.com>
2017-05-06 17:04:46 -0600

    added single team path

--- src/main/java/com/example/TeamController.java
+++ src/main/java/com/example/TeamController.java
@@ -1,19 +1,24 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TeamController {
 
-	
-	@Autowired 
+	@Autowired
 	TeamRepository teamRepository;
-	
-	  @RequestMapping("/teams")
-	  public Iterable<Team> getTeams() {
-	  	return teamRepository.findAll();
-	  }
+
+	@RequestMapping("/teams")
+	public Iterable<Team> getTeams() {
+		return teamRepository.findAll();
+	}
+
+	@RequestMapping("/teams/{id}")
+	public Team getTeam(@PathVariable Long id) {
+		return teamRepository.findOne(id);
+	}
 
 }
