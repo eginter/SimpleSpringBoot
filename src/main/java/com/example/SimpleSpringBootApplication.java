40d0c619809c560f16d82d17a74ff4a03817ca9b
elijah <ginterelijah@gmail.com>
2017-05-06 17:01:20 -0600

    Added /teams mapping & JPA annotations

--- src/main/java/com/example/SimpleSpringBootApplication.java
+++ src/main/java/com/example/SimpleSpringBootApplication.java
@@ -1,12 +1,41 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimpleSpringBootApplication {
 
+	@Autowired
+	TeamRepository teamRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleSpringBootApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		List<Team> list = new ArrayList<>();
+
+		/**
+		 * initialize a couple in memory Team objects.
+		 */
+		Team team = new Team();
+		team.setLocation("Harlem");
+		team.setName("Globetrotters");
+		list.add(team);
+
+		team = new Team();
+		team.setLocation("Washington");
+		team.setName("Generals");
+		list.add(team);
+
+		teamRepository.save(list);
 	}
 }
---
+++ src/main/java/com/example/TeamController.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeamController {
+
+	
+	@Autowired 
+	TeamRepository teamRepository;
+	
+	  @RequestMapping("/teams")
+	  public Iterable<Team> getTeams() {
+	  	return teamRepository.findAll();
+	  }
+
+}
